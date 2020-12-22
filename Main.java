3ef2438f167a1655e545011ca81357d69c267a8b
Abhinav297 <73228836+Abhinav297@users.noreply.github.com>
2020-12-22 22:44:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,30 +1,26 @@
-package com.upgrad;
+package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-        Address address1 = new Address("Mirzapur","UP", "Vasaliganj"); //Creating Address object
-        System.out.println(address1.getAddressDetails());
-        address1.updateAddressDetails("Lalbangla", "Kanpur", "UP");//Updating Address object
-        System.out.println(address1.getAddressDetails());
+        Passenger passenger = new Passenger("10th Street", "Banglore", "Karnataka" + "", "Vikas", "1234567", "asdfg@gmail.com");
+        System.out.println(passenger.getAddressDetails());
+        System.out.println(passenger.getContactDetails());
+        System.out.println(passenger.toString());
 
+        Flight flight = new Flight("1234","Upgrad airlines",120,20);
+        RegularTicket regularTicket = new RegularTicket("Reg-123", "BOM", "BLR","12/12/12","12/12/21",false,flight,passenger,"Fooding");
+    }
 
-        Contact contact1 = new Contact("Raj", "1234567890", "something@gmail.com");//Creating Contact object
-        System.out.println(contact1.getContactDetails());
-        contact1.updateContactDetails("Rajesh", "0987654321", "no@gmail.com");//Updating Contact object
-        System.out.println(contact1.getContactDetails());
-
-        Flight flight1 = new Flight("FED645", "Boieng", 125, 0);
-        System.out.println(flight1.getFlightDetails());
-        System.out.println(flight1.getBookedSeats());
-        System.out.println(flight1.checkAvailability());
-
-        Passenger passenger1 = new Passenger(567, address1,contact1);
-        System.out.println(passenger1.getAddress());
-
-        Ticket ticket1 = new Ticket("123456", "Mumbai", "banglore");
-        System.out.println(ticket1.getFlightDuration());
-
-
-	}
+    public static void printTicketDetails(Ticket ticket){
+        System.out.println(ticket.getPnr());
+    }
 }
+
+
+
+
+
+
+
+
